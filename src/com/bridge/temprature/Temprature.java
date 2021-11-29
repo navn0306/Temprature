ebaf2278258de00a5ad68d66fd919d71839106e6
Navneet <navn0306@gmail.com>
2021-11-29 19:35:50 +0530

    ADD: Added the program to covert the Ttemprature in celsius or Viceversa

---
+++ src/com/bridge/temprature/Temprature.java
@@ -0,0 +1,8 @@
+package com.bridge.temprature;
+
+public class Temprature {
+    public static void main(String[] args) {
+        System.out.println("Welcome, In this program, given temperature in fahrenheit as input, outputs the temperature in Celsius or viceversa.");
+    }
+
+}
