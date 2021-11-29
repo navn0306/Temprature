fe0ed72bedb9cfa260551b298ec7b67998b5519f
Navneet <navn0306@gmail.com>
2021-11-29 19:47:53 +0530

    ADD: Added the program to convert Fahrenheit to Celsius or ViceVersa

--- src/com/bridge/temprature/Temprature.java
+++ src/com/bridge/temprature/Temprature.java
@@ -1,8 +1,32 @@
 package com.bridge.temprature;
+import java.util.Scanner;
 
 public class Temprature {
-    public static void main(String[] args) {
-        System.out.println("Welcome, In this program, given temperature in fahrenheit as input, outputs the temperature in Celsius or viceversa.");
+     public static void calculateTemprature() {
+        float fahrenheit, celsius;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose type of conversion \n 1.Fahrenheit to Celsius  \n 2.Celsius to Fahrenheit");
+        int ch = sc.nextInt();
+        switch (ch) {
+            case 1:
+                System.out.println("Enter  Fahrenheit temperature");
+                fahrenheit = sc.nextFloat();
+                celsius = (fahrenheit - 32) * 5 / 9;
+                System.out.println("Celsius temperature is = " + celsius);
+                break;
+            case 2:
+                System.out.println("Enter  Celsius temperature");
+                celsius = sc.nextFloat();
+                fahrenheit = ((9 * celsius) / 5) + 32;
+                System.out.println("Fahrenheit temperature is = " + fahrenheit);
+                break;
+            default:
+                System.out.println("please choose valid choice");
+        }
     }
 
+    public static void main(String arg[]) {
+        calculateTemprature();
+
+    }
 }
